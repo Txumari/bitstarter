8dfbebae93097de8a4ed405185240b7a772fa0d5
Jesus M Eraso <txumaritig@gmail.com>
2013-07-10 10:11:41 +0000

    updated web.js errors

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
 //   response.send('hola');
-    reponse.send(fs.readFileSync('index.html').toString());
+    response.send(fs.readFileSync('index.html').toString());
 
 });
 
