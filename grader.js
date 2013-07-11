d8600aee80950a1d2204ef52388e7ad852760dd6
Jesus M Eraso <txumaritig@gmail.com>
2013-07-11 16:50:47 +0000

    update grader.js with --url  option

--- grader.js
+++ grader.js
@@ -24,9 +24,13 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var restler = requier('restler');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+//var URL_DEFAULT = "www.google.es";
+var fs = require('fs');
+var outfile = "url_file.html";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,11 +69,25 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Url to webpage')
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.url){
+	rest.get(program.url).on('complete', function(result, response){
+//	    console.log(result);
+	    fs.writeFileSync(outfile,result);
+	    var checkJson = checkHtmlFile(outfile,program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	    fs.writeFileSync('out.json',outJson);
+	});   
+    }else{
+	console.log(program.file);
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
